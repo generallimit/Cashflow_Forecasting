6ace7e524c69840c0c2da1716b3134f95b8f8484
Jeff Patton <patton.jeffery@gmail.com>
2025-04-23 13:45:29 -0600

    remove outputs

--- excelCSV_convert.py
+++ excelCSV_convert.py
@@ -1,5 +1,5 @@
 import pandas as pd
-read_file = pd.read_excel('selectionscashflows_2024.xlsx')
+read_file = pd.read_excel('cashflows_2024.xlsx')
 
 read_file.to_csv ('selectionscashflows_2024.csv',  
                   index = None, 
@@ -7,7 +7,7 @@ read_file.to_csv ('selectionscashflows_2024.csv',
     
 # read csv file and convert  
 # into a dataframe object 
-df = pd.DataFrame(pd.read_csv('selectionscashflows_2024.csv')) 
+df = pd.DataFrame(pd.read_csv('flows_2024.csv')) 
   
 # show the dataframe 
 df
